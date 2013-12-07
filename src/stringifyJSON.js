85aae5b19f3bbdf2a8a81acc75aa410d29d51fc8
Daniel Almaguer <danielalmaguer@gmail.com>
2013-12-07 16:17:41 -0600

    Added stringifyJSON solution (ToDo: Refactor)

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,29 @@
 
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
-  // your code goes here
+  // ToDO: REFACTOR
+  if (obj === null || typeof obj === 'number' || typeof obj === 'boolean') {
+    return String(obj);
+  }
+
+  var type = typeof obj;
+
+  if (type === 'string') {
+    return '"' + obj + '"';
+  } else if (Array.isArray(obj)) {
+    var arr = [];
+    for (var i = 0; i < obj.length; i++) {
+      arr.push(stringifyJSON(obj[i]));
+    }
+    return '[' + arr + ']';
+  } else if (type === 'object') {
+    var arr = [];
+    for (i in obj) {
+      var value = stringifyJSON(obj[i]);
+      if(value) {
+        arr.push(stringifyJSON(i) + ':' + value);
+      }
+    }
+    return '{' + arr + '}';
+  }
 };
